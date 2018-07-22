aac472c0a6935a3971bfba176d358bb2a4c49678
dmitriy <dmitr.romashov@gmail.com>
2018-07-22 23:42:55 +0300

    added bot description and start command processing

--- src/main/java/com/dmitrromashov/App.java
+++ src/main/java/com/dmitrromashov/App.java
@@ -1,9 +1,27 @@
 package com.dmitrromashov;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.starter.EnableTelegramBots;
+
+@SpringBootApplication
+@EnableTelegramBots
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new WeatherBot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+
     }
 }
